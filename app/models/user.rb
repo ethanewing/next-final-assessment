7d667b711d7adf4a500ab79dd3bb6fedd5d66db8
Ethan Ewing <ethanewing52@gmail.com>
2018-05-04 14:38:57 +0800

    created database; user migration; user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
 end
